247f89b1ca254d8a0f74d06a6f90e21e05f513dc
sundaysantakim-del <sundaysantakim@gmail.com>
2026-06-03 16:31:44 +0900

    git add . git commit -m "Refine compare CTA background" git push

--- src/App.js
+++ src/App.js
@@ -427,7 +427,7 @@ function ProductCompareScreen({
   return h("div", { className: "figma-result-screen" }, [
     h("img", {
       className: "figma-result-image",
-      src: selected ? `${A}feature3-compare_4.svg?v=2` : `${A}feature3-compare_2.svg?v=shadowsoft`,
+      src: selected ? `${A}feature3-compare_4.svg?v=sharp1` : `${A}feature3-compare_2.svg?v=shadowsoft`,
       alt: "제품 비교 화면",
       key: "image",
     }),
@@ -466,7 +466,7 @@ function ProductCompareScreen({
         onClick: () => onToggleProduct(product),
         style: { top: `${compareSelectButtonTops[product.rowIndex]}px` },
         key: `selected-${product.id}`,
-      }, h("img", { src: `${A}compare-selected-cta.svg?v=slot1`, alt: "선택됨" }))),
+      }, h("img", { src: `${A}compare-selected-cta.svg?v=sharp1`, alt: "선택됨" }))),
       compareSelectButtonTops.map((top, index) => {
         const product = compareDemoProducts.find((item) => item.rowIndex === index);
         return h("button", {
